1ae989ec76f28e899098363fe64e3142fc8f1244
peter <peter@vapewholesaleusa.com>
2024-07-19 16:10:21 -0700

    use google api

--- src/index.ts
+++ src/index.ts
@@ -27,6 +27,68 @@ export interface Env {
 
 export default {
 	async fetch(request: Request, env: Env, ctx: ExecutionContext): Promise<Response> {
-		return new Response('Hello World!');
+		const API_KEY = 'AIzaSyC62ULyxZmIBwjOHWt2O0Hfkj_hwBdIYc4';
+		const SEARCH_ENGINE_ID = '76e35492042ab4de6';
+		interface MagentoProductType {
+			"catalog_product_entity|sku": string,
+			"catalog_product_entity|name": string,
+			"sales_order|entity_id__cnt": string
+		}
+		interface MagentoResponseBodyType {
+			columns: Object[],
+			rows: MagentoProductType[]
+		}
+
+		interface GoogleSearchResponseBodyType {
+			items: {
+				title: string,
+				link: string,
+				snippet: string,
+			}[]
+		}
+		interface ProductLinkType {
+			title: string,
+			link: string
+		}
+		interface ProductLinksType {
+			name: string,
+			links: ProductLinkType[]
+		}
+		const productListUrl = "https://vapewholesaleusa.com/report/view/59035b598250d9a84f28300693bdf88f2109b005.json"
+
+		async function googleSearch(query: string): Promise<ProductLinkType[]> {
+			console.log('searching :', query)
+			const url = `https://www.googleapis.com/customsearch/v1?key=${API_KEY}&cx=${SEARCH_ENGINE_ID}&q=${encodeURIComponent(query)}`;
+
+			try {
+				const response = await fetch(url);
+				const data = await response.json() as GoogleSearchResponseBodyType;
+				if (data.items && data.items.length > 0) {
+					return data.items.slice(0, 5).map(item => ({
+						title: item.title,
+						link: item.link,
+					}));
+				} else {
+					return [];
+				}
+			} catch (error) {
+				console.error('Error performing Google search:', error);
+				return [];
+			}
+		}
+
+		const response = await (fetch(productListUrl) as unknown) as Response
+		const data = await response.json() as MagentoResponseBodyType
+		const productList = data.rows
+		console.log('productList.length', productList.length)
+		let productLinks: ProductLinksType[] = []
+		for (const product in productList.slice(0, 1)) {
+			const productName = productList[product]["catalog_product_entity|name"]
+			const links = await googleSearch(productName);
+			productLinks.push({ name: productName, links: links })
+		}
+		console.log('productLinks', productLinks)
+
+		return Response.json(productLinks)
 	},
 };
